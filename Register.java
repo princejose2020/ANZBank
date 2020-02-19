3dfbf49825f84511513cc7a30e5c8e484cda2702
princejose2020 <prjo526@ess.ais.ac.nz>
2020-02-20 00:48:15 +1300

    Register update

--- Register.java
+++ Register.java
@@ -2,6 +2,6 @@ public clas Register
 {
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome Register Page !!!!");
+		System.out.println("Welcome Register Pages !!!!");
 	}
 }
