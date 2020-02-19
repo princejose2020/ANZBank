e2c8bdcc1dff1fd7293e2cb1f25ef4f5221554e7
princejose2020 <prjo526@ess.ais.ac.nz>
2020-02-19 15:24:32 +1300

    Prince V1

---
+++ Login.java
@@ -0,0 +1,7 @@
+public clas Login 
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome Login Page !!!!");
+	}
+}
---
+++ Register.java
@@ -0,0 +1,7 @@
+public clas Register
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome Register Page !!!!");
+	}
+}
