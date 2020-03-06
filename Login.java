c437ccfe8705dab664c5967ac07c095b1e91817c
princejose2020 <60952653+princejose2020@users.noreply.github.com>
2020-03-07 00:05:38 +1300

    Update Login.java

--- Login.java
+++ Login.java
@@ -2,6 +2,6 @@ public class Login
 {
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome Login Page !!!!");
+		System.out.println("Welcome Login Page is another page!!!!");
 	}
 }
